cc4faea95f998b388c07b7abb91679df94247493
Aleksander Piciga <aleksander.piciga@gmail.com>
2022-12-08 15:47:23 +0100

    day06 oneline

--- day06/day06.py
+++ day06/day06.py
@@ -1,13 +1,13 @@
+# buffer = open("input.txt", "r").readline()
+
+# def find_marker(buffer, marker_len):
+#     for i in range(marker_len, len(buffer) + 1):
+#         if len(set(buffer[i - marker_len:i])) == marker_len:
+#             return i
+
+# print("Part 1:", find_marker(buffer, 4))
+# print("Part 2:", find_marker(buffer, 14))
+
 buffer = open("input.txt", "r").readline()
-
-def find_marker(buffer, marker_len):
-    marker = [None for _ in range(marker_len)]
-    for i, char in enumerate(buffer):
-        marker[i % len(marker)] = char
-        if i < len(marker):
-            continue
-        if len(set(marker)) == marker_len:
-            return i + 1
-
-print("Part 1:", find_marker(buffer, 4))
-print("Part 2:", find_marker(buffer, 14))
+print("Part 1:", next(i for i in range(4, len(buffer) + 1) if len(set(buffer[i - 4:i])) == 4))
+print("Part 2:", next(i for i in range(14, len(buffer) + 1) if len(set(buffer[i - 14:i])) == 14))
