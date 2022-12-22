ad885007a494563f13194a8051b93ef2baeaacc7
Aleksander Piciga <aleksander.piciga@gmail.com>
2022-12-22 20:09:38 +0100

    unfinished improvment of 16

--- day16/day16.py
+++ day16/day16.py
@@ -1,39 +1,24 @@
 import re
-from functools import lru_cache
 
+valve_connections = {}
+valve_flow = {}
+with open("input.txt") as f:
+    for line in f:
+        matches = re.findall(r".*([A-Z][A-Z]).*=([0-9]+).*valves? ((?: ?[A-Z],?)*)", line)[0]
+        valve_flow[matches[0]] = int(matches[1])
+        valve_connections[matches[0]] = list(matches[2].split(", "))
 
-def read_valves():
-    valve_connections = {}
-    valve_flow = {}
-    with open("input.txt") as f:
-        for line in f:
-            matches = re.findall(r".*([A-Z][A-Z]).*=([0-9]+).*valves? ((?: ?[A-Z],?)*)", line)[0]
-            valve_flow[matches[0]] = int(matches[1])
-            valve_connections[matches[0]] = list(matches[2].split(", "))
-    return valve_connections, valve_flow
-
-def lru_wrapper(func):
-    def wrapper(*args, **kwargs):
-        return func(*args, **kwargs)
-    return wrapper
-    
-@lru_wrapper
-@lru_cache
-def release_flow(opened_valves, current_valve, total_flow, minutes):
+def max_water(current, minutes, flowrate, water, node_flow):
     if minutes == 0:
-        return total_flow
-    
-    released_flow = total_flow
-    if current_valve  not in opened_valves and valve_flow[current_valve] > 0:
-        opened_valves.add(current_valve)
-        released_flow = max(released_flow, release_flow(opened_valves, current_valve, total_flow + valve_flow[current_valve] * (minutes -1), minutes - 1))
-        opened_valves.remove(current_valve)
-    
-    for valve in valve_connections[current_valve]:
-        move_released_flow = release_flow(opened_valves, valve, total_flow, minutes - 1)
-        released_flow = max(released_flow, move_released_flow)
-    
-    return released_flow
+        return water
+    if node_flow.get(current, 0) >= flowrate:
+        return water
+    node_flow[current] = node_flow.get(current, 0) + valve_flow[current]
+    water += valve_flow[current]
 
-valve_connections, valve_flow = read_valves()
-print(release_flow(set(), "AA", 0, 30))
+    w = 0
+    for valve in valve_connections[current]:
+        w = max(w, max_water(valve, minutes - 1, flowrate, water, node_flow))
+    
+        
+print("Part 1:", max_water("AA", 30))
